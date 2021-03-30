19999e8dabcdc7a010506eba7b03ef45e0a4b753
Andriy <ak8022@bs.amalnet.k12.il>
2021-03-30 13:55:58 +0300

    A commit

---
+++ app/src/main/java/com/example/firebaseproject/Vaccine.java
@@ -0,0 +1,33 @@
+package com.example.firebaseproject;
+
+public class Vaccine
+{
+    private int date;
+    private String location;
+
+    public Vaccine(int date, String location)
+    {
+        this.date=date;
+        this.location=location;
+    }
+
+    public void setDate(int date)
+    {
+        this.date=date;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location=location;
+    }
+
+    private int getDate()
+    {
+        return date;
+    }
+
+    private String getLocation()
+    {
+        return location;
+    }
+}
