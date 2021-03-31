e85afed8ae428298347730c91a1b3675cadb764c
Andriy <ak8022@bs.amalnet.k12.il>
2021-03-31 15:06:10 +0300

    A commit

---
+++ app/src/main/java/com/example/firebaseproject/SecondVaccine.java
@@ -0,0 +1,16 @@
+package com.example.firebaseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondVaccine extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_vaccine);
+    }
+}
