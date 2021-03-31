2a2f22aca82d79f09e15279199e01264d1d1fab1
Andriy <ak8022@bs.amalnet.k12.il>
2021-03-31 15:35:20 +0300

    A commit

--- app/src/main/java/com/example/firebaseproject/SecondVaccine.java
+++ app/src/main/java/com/example/firebaseproject/SecondVaccine.java
@@ -1,15 +1,11 @@
 package com.example.firebaseproject;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class SecondVaccine extends AppCompatActivity
 {
-    AlertDialog.Builder adb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,17 +15,5 @@ public class SecondVaccine extends AppCompatActivity
 
         Toast.makeText(this,"Press on a student and enter info about his second vaccine", Toast.LENGTH_LONG).show();
         Toast.makeText(this,"Only students who got just one vaccine appear on this screen", Toast.LENGTH_LONG).show();
-        adb=new AlertDialog.Builder(this);
-        adb.setTitle("Press on a student and enter info about his second vaccine");
-        adb.setPositiveButton("OK", new DialogInterface.OnClickListener()
-        {
-            @Override
-            public void onClick(DialogInterface dialog, int which)
-            {
-                //exit
-            }
-        });
-        AlertDialog a=adb.create();
-        a.show();
     }
 }
