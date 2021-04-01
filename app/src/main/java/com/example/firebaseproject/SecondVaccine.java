a04fda4403cab18f08984538e40111ca1c62e97d
Andriy <ak8022@bs.amalnet.k12.il>
2021-04-01 15:10:37 +0300

    A commit

--- app/src/main/java/com/example/firebaseproject/SecondVaccine.java
+++ app/src/main/java/com/example/firebaseproject/SecondVaccine.java
@@ -91,32 +91,6 @@ public class SecondVaccine extends AppCompatActivity implements View.OnCreateCon
         Toast.makeText(this,"Only students who got just one vaccine appear on this screen", Toast.LENGTH_LONG).show();
     }
 
-    /*
-
-     * Callback method to be invoked when an item in this AdapterView has
-     * been clicked.
-     * <p>
-     * Implementers can call getItemAtPosition(position) if they need
-     * to access the data associated with the selected item.
-     *
-     * @param parent   The AdapterView where the click happened.
-     * @param view     The view within the AdapterView that was clicked (this
-     *                 will be a view provided by the adapter)
-     * @param position The position of the view in the adapter.
-     * @param id       The row id of the item that was clicked.
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-    {
-        name=studentsNames.get(position);
-        student=studentsData.get(position);
-        getDate();
-        studentsNames.remove(name);
-        studentsData.remove(student);
-        adp.notifyDataSetChanged();
-    }
-    */
-
     public void getDate()
     {
         adb=new AlertDialog.Builder(this);
@@ -173,10 +147,6 @@ public class SecondVaccine extends AppCompatActivity implements View.OnCreateCon
         name=studentsNames.get(position);
         student=studentsData.get(position);
 
-        if(studentsNotSecond.get(position))
-        {
-            menu.add("Add Second vaccine");
-        }
         if(!student.getCant())
         {
             menu.add("Correct First date");
@@ -186,6 +156,10 @@ public class SecondVaccine extends AppCompatActivity implements View.OnCreateCon
         {
             menu.add("Correct second date");
             menu.add("Correct second location");
+        }
+        else if(studentsNotSecond.get(position))
+        {
+            menu.add("Add Second vaccine");
         }
     }
 
