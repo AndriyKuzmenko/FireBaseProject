4ba263c942cca0a7f051c49d449aa53f81cff26e
Andriy <ak8022@bs.amalnet.k12.il>
2021-04-01 18:19:28 +0300

    A commit

--- app/src/main/java/com/example/firebaseproject/SortActivity.java
+++ app/src/main/java/com/example/firebaseproject/SortActivity.java
@@ -1,13 +1,19 @@
 package com.example.firebaseproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -22,11 +28,50 @@ public class SortActivity extends AppCompatActivity
     AlertDialog.Builder adb;
     ArrayAdapter<String> adp;
     int position;
+    ListView studentsList1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sort);
+
+        studentsList1=(ListView)findViewById(R.id.studentsList1);
+        database=FirebaseDatabase.getInstance("https://homework-9f97d-default-rtdb.firebaseio.com/");
+        refStudents=database.getReference("Students");
+        studentsNames=new ArrayList<>();
+        studentsData=new ArrayList<>();
+
+        refStudents.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for(DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    String temp=data.getKey();
+                    Student t=data.getValue(Student.class);
+
+                    studentsNames.add(temp);
+                    studentsData.add(t);
+                }
+                showData();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
+    }
+
+    public void showData()
+    {
+        adp=new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, studentsNames);
+        studentsList1.setAdapter(adp);
+
+        Toast.makeText(this,"Press on a student to see his data", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"You can filter the students using the filters on the bottom of the sreen.", Toast.LENGTH_LONG).show();
     }
 }
