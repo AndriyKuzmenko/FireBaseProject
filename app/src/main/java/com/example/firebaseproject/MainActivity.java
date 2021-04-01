eae3f5cc713e9c97d5d836c62a770e0d3c07cf7b
Andriy <ak8022@bs.amalnet.k12.il>
2021-04-01 17:46:53 +0300

    A commit

--- app/src/main/java/com/example/firebaseproject/MainActivity.java
+++ app/src/main/java/com/example/firebaseproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebaseproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +13,16 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -24,6 +32,8 @@ public class MainActivity extends AppCompatActivity
     DatabaseReference myRef;
     String name1;
     Student st;
+    FirebaseDatabase database;
+    ArrayList<String> studentsNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,13 +46,40 @@ public class MainActivity extends AppCompatActivity
         grade=(EditText)findViewById(R.id.grade);
         class1=(EditText)findViewById(R.id.class1);
         canVaccinate=(CheckBox)findViewById(R.id.canVaccinate);
+
+        database=FirebaseDatabase.getInstance("https://homework-9f97d-default-rtdb.firebaseio.com/");
+        myRef=database.getReference("Students");
+        studentsNames=new ArrayList<>();
+
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for(DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    String temp=data.getKey();
+                    studentsNames.add(temp);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
     }
 
     public void save(View view)
     {
-        FirebaseDatabase database=FirebaseDatabase.getInstance("https://homework-9f97d-default-rtdb.firebaseio.com/");
         name1=firstName.getText().toString()+" "+lastName.getText().toString();
-        myRef=database.getReference("Students");
+        if(studentsNames.contains(name1))
+        {
+            Toast.makeText(this,"You have already entered "+name1+"! Please enter another student!",Toast.LENGTH_LONG).show();
+            return;
+        }
         boolean bo=canVaccinate.isChecked();
         st=new Student(Integer.parseInt(grade.getText().toString()),Integer.parseInt(class1.getText().toString()),!bo);
         if(bo)
