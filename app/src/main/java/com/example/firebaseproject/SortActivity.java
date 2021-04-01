6b173bc942ef617226402bd86b56e017883a561a
Andriy <ak8022@bs.amalnet.k12.il>
2021-04-01 18:12:01 +0300

    A commit

--- app/src/main/java/com/example/firebaseproject/SortActivity.java
+++ app/src/main/java/com/example/firebaseproject/SortActivity.java
@@ -1,11 +1,27 @@
 package com.example.firebaseproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class SortActivity extends AppCompatActivity
 {
+    ArrayList<String> studentsNames;
+    ArrayList<Student> studentsData;
+    String name;
+    Student student;
+    FirebaseDatabase database;
+    DatabaseReference refStudents;
+    AlertDialog.Builder adb;
+    ArrayAdapter<String> adp;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
